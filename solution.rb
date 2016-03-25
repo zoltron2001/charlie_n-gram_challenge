12a19da443602dd678468f627abcd6477821f1b9
Zollie Barnes <zolliebarnes@gmail.com>
2016-03-25 15:40:15 -0500

    update to use slice rather than hard coding answers

--- solution.rb
+++ solution.rb
@@ -1,3 +1,40 @@
+
+# Initial Solution
+
+def generate_ngram(string)
+  initial_array = string.split(" ")
+  p return_ngram_array(initial_array)
+  return_ngram_array(initial_array)
+end
+
+def return_ngram_array(array)
+    output_array = []
+    index = 0
+    until index == array.length
+      # adds unigram to output
+      output_array << array[index]
+      # adds bigram to output if next element exists
+      # using double bang to turn 'truthy nils' into false
+      if !!array[index+1]
+        output_array << array.slice(index,2).join(" ")
+      end
+      # adds trigram to output if next element exists
+      if !!array[index+2]
+        output_array << array.slice(index,3).join(" ")
+      end
+      index += 1
+    end
+    output_array
+end
+
+# test
+
+if generate_ngram("Make a killer impression.") == ["Make", "Make a", "Make a killer", "a", "a killer", "a killer impression.", "killer", "killer impression.", "impression."]
+  p "Charlie Test Passed"
+else
+  p "Charlie Test Failed"
+end
+
 =begin
 Pseudocode
 
@@ -21,38 +58,3 @@ Requirements
 * Include tests
 
 =end
-
-# Initial Solution
-
-def generate_ngram(string)
-  initial_array = string.split(" ")
-  return_ngram_array(initial_array)
-end
-
-def return_ngram_array(array)
-    output_array = []
-    index = 0
-    until index == array.length - 1
-      # adds unigram to output
-      output_array << array[index]
-      # adds bigram to output if next element exists
-      # using double bang to turn 'truthy nils' into false
-      if !!array[index+1]
-        output_array << array[index]+" "+array[index+1]
-      end
-      # adds trigram to output if next element exists
-      if !!array[index+2]
-        output_array << array[index]+" "+array[index+1]+" "+array[index+2]
-      end
-      index += 1
-    end
-    output_array
-end
-
-# tests
-
-if generate_ngram("Make a killer impression.") == ["Make", "Make a", "Make a killer", "a", "a killer", "a killer impression.", "killer", "killer impression.", "impression."]
-  p "Charlie test Passed"
-else
-  p "Charlie test Failed"
-end
